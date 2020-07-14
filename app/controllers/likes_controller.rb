a944f44bdf843b6aa1c99a760bbd1b506ce9fcc7
akebonoex1008 <tx.yagi@gmail.com>
2020-07-14 19:47:45 +0900

    completion

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,18 @@ class LikesController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
 		@post.like(current_user)
-		redirect_to @post.user
+		respond_to do |format|
+      format.html { redirect_to @post.user }
+      format.js
+    end
   end
 
 	def destroy
 		@post = Like.find(params[:id]).post
 		@post.unlike(current_user)
-		redirect_to @post.user
+		respond_to do |format|
+      format.html { redirect_to @post.user }
+      format.js
+    end
   end
 end
