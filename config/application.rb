82a3d11528a74d10d1bd8e846994faf248416fc0
akebonoex1008 <tx.yagi@gmail.com>
2020-08-01 17:07:43 +0900

    twitter api　によるログイン機能の実装　headerの編集

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Myapp
   class Application < Rails::Application
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
@@ -15,5 +16,13 @@ module Myapp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_view.embed_authenticity_token_in_remote_forms = true
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    #　以下の記述を追記する(設定必須)
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+    #　#　以下の記述を追記する
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200719020659) do
+ActiveRecord::Schema.define(version: 20200730155250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,11 @@ ActiveRecord::Schema.define(version: 20200719020659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image"
+    t.string "provider"
+    t.string "uid"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
